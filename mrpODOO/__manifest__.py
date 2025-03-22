484b7eab658017324e0cb766b5c38993268c0137
ealtiparmak <ejder.altiparmak@gmail.com>
2025-03-22 09:17:31 +0000

    Update manifest to fix visibility

--- mrpODOO/__manifest__.py
+++ mrpODOO/__manifest__.py
@@ -2,9 +2,14 @@
     'name': 'BOM Button for Sales Orders',
     'version': '1.0',
     'depends': ['sale', 'mrp'],
+    'author': 'Ejder Altiparmak',
+    'category': 'Sales',
+    'summary': 'Adds a button to fetch BOM components on Sale Orders',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
     'data': [
         'views/sale_order_view.xml',
     ],
-    'installable': True,
-    'auto_install': False,
 }
+
