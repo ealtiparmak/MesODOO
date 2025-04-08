981efd129272b400c8a60d7d6ab533458e8ca9fe
Ejder ALTIPARMAK <ejder.altiparmak@gmail.com>
2025-04-08 12:28:04 +0300

    Create __manifest__.py

---
+++ custom/custom-addons/test_hello/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Test Hello",
+    "version": "1.0",
+    "summary": "Test modülü - sadece manifest",
+    "depends": [],
+    "data": [],
+    "installable": true,
+    "application": false
+}
