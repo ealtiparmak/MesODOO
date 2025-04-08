e745e0c1bd5fc24e2eeb729313f8e153742f2ef1
Ejder ALTIPARMAK <ejder.altiparmak@gmail.com>
2025-04-08 11:59:01 +0300

    Create manifest.py

---
+++ custom/custom-addons/attribute_name_fix/manifest.py
@@ -0,0 +1,10 @@
+{
+    "name": "Attribute Name Fix",
+    "version": "1.0",
+    "summary": "Renk niteliği görüntüsünü sadeleştirir",
+    "depends": ["product"],
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+    "application": False
+}
