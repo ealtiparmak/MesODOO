c88e3ebd20c241e2e4d70c6bb6a47bcb51c82447
Ejder ALTIPARMAK <ejder.altiparmak@gmail.com>
2025-04-13 13:06:26 +0300

    Delete custom/custom-addons/test_hello directory

--- custom/custom-addons/test_hello/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    "name": "Test Hello",
-    "version": "1.0",
-    "summary": "Test modülü - sadece manifest",
-    "depends": [],
-    "data": [],
-    "installable": true,
-    "application": false
-}
